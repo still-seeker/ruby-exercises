70ee037990308b61f2da81f1b5ab1c54af46552e
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-03-04 08:53:09 +0200

    Finish the nested array exercise

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,21 +11,28 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  return Array.new(number_of_rows) {Array.new(seats_per_row)}
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  return chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  return chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
   # take a chart and delete the seat at seat_index of the row at row_index of
   # the chart, then return the chart
+  chart[row_index].delete_at(seat_index)
+  return chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
 end
@@ -33,10 +40,21 @@ end
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  return chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
+  count = 0
+  chart.each_with_index do |row, seat|
+    row.each_with_index do |col, seat_index|
+      if chart[seat][seat_index] == nil then
+        count += 1
+      end
+    end
+  end
+  return count
 
   # NOTE: `chart` should **not** be mutated
 end
@@ -55,4 +73,6 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  array_of_hash_objects.find{ |hash| hash[:is_my_favorite?]}
+    
 end
